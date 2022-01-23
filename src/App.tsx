4fc76164b0369df569770505d51ef7bcf65aaabe
Edwin Bryan <bbryan211@gmail.com>
2022-01-22 20:16:40 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import eyesc from './assets/img/eyesc.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        {/* <img src={eyesc} className="eyes" alt="logo" /> */}
+        <div className="eyes"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
